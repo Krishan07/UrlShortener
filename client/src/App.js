3b7a580d142fe948a854649b32025027987622da
Krishan <kb2796132@gmail.com>
2021-09-05 13:23:17 -0400

    september re-upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Notes from './pages/Notes'
-import Create from './pages/Create'
 
 function App() {
   return (
@@ -8,9 +7,6 @@ function App() {
       <Switch>
         <Route exact path="/">
           <Notes />
-        </Route>
-        <Route path="/create">
-          <Create />
         </Route>
       </Switch>
     </Router>
